a1cc931add36e54f1692b98ee1d97bdd7fa4c64d
Maxwell Bourcier <maxwellbourcier@gmail.com>
2023-10-16 20:19:18 -0400

    GameEnum class initial push

---
+++ modules/GameEnums.py
@@ -0,0 +1,30 @@
+from enum import Enum
+import pygame
+
+class Direction (Enum):
+    """
+    An Enum class that handles user input conversion from pygame
+    """
+    LEFT = pygame.K_LEFT
+    RIGHT = pygame.K_RIGHT
+    UP = pygame.K_UP
+    DOWN = pygame.K_DOWN
+
+class GameLevel (Enum): 
+    """
+    An Enum class that represents the overall game difficulty
+    """
+    BEGINNER = 0
+    INTERMEDIATE = 1
+    HARD = 2
+    ADVANCED = 3
+
+class CellType (Enum):
+    """
+    An Enum class that represents the type of cell to fill the grid
+    """
+    EMPTY = 0
+    BLOCKED = 1
+    PLAYER = 2
+    PORTAL = 3
+    POWER_UP = 4
