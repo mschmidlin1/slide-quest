510a54cd2edfff10d0682e3868809b598e73eff0
Maxwell Bourcier <maxwellbourcier@gmail.com>
2023-10-16 21:52:36 -0400

    pygame file creation

--- SlideQuest.py
+++ SlideQuest.py
@@ -1,8 +1,24 @@
-
-
+import pygame
+from modules.SQGame import SQGame
+from modules.configs import WINDOW_WIDTH, WINDOW_HEIGHT
+import sys
 
 game = SQGame()
+
+BLACK = (0, 0, 0)
+
+pygame.init()
+SCREEN = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+CLOCK = pygame.time.Clock()
+SCREEN.fill(BLACK)
 
 
 while True:
     game.DrawBoard()
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    pygame.display.update()
--- modules/SQGame.py
+++ modules/SQGame.py
@@ -1,16 +1,21 @@
 import pygame
-#from configs import WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_DIMENSIONS
+from modules.configs import WINDOW_WIDTH, WINDOW_HEIGHT, CELL_WIDTH, CELL_HEIGHT
 
-"""
+BLACK = (0, 0, 0)
+WHITE = (200, 200, 200)
+
+CLOCK = pygame.time.Clock()
+WHITE = (255, 255, 255)
 SCREEN = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
-"""
+
 class SQGame:
-    def __init__(self, GRID_WIDTH, GRID_HEIGHT, CELL_DIMS): #initial variables specifically for the creation of the grid (add more later)
+    def __init__(self): #initial variables specifically for the creation of the grid (add more later)
         pass 
 
-def DrawBoard():
-    CELL_DIMS = 32
+    def DrawBoard(self):
+        CELL_DIMS = 32
 
-    for x in range(0, configs.WINDOW_WIDTH, GRIDSIZE):
-        for y in range(0, configs.WINDOW_HEIGHT, GRIDSIZE):
-            continue 
+        for x in range(0, WINDOW_WIDTH, CELL_WIDTH):
+            for y in range(0, WINDOW_HEIGHT, CELL_HEIGHT):
+                rect = pygame.Rect(x, y, CELL_WIDTH, CELL_HEIGHT)
+                pygame.draw.rect(SCREEN, WHITE, rect, 1)
