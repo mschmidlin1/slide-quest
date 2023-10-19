c55177f5daabbf28a725b3a7051e5a6a33dcc6eb
Maxwell Bourcier <maxwellbourcier@gmail.com>
2023-10-19 16:31:45 -0400

    removal of SQGame - main is now more robust

--- SlideQuest.py
+++ SlideQuest.py
@@ -1,30 +1,60 @@
 import pygame
-from modules.SQGame import SQGame
-from modules.configs import WINDOW_WIDTH, WINDOW_HEIGHT
+from modules.configs import *
 import sys
-
-game = SQGame()
-
-BLACK = (0, 0, 0)
-
-
 
 class Game:
     def __init__(self):
         pygame.init()
-        pygame.display.set_caption(title="Slide Quest")
+        pygame.display.set_caption(TITLE)
+
         self.screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
         self.clock = pygame.time.Clock()
         self.load_data()
-        self.screen.fill(BLACK)
+
+    def load_data(self):
+        #load map
+        pass
+
+    def new(self):
+        #initialization of all variables for new games
+        pass
 
 
-    while True:
-        game.DrawBoard()
+    def run(self):
+        #game loop - set self.playing to False to end game
 
+        self.playing = True
+
+        while self.playing:
+            self.events()
+            self.update()
+            self.draw()
+
+    def events(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
+                self.quit()
+    def draw_grid(self):
+        for x in range(0, WINDOW_WIDTH, CELL_WIDTH):
+            pygame.draw.line(self.screen, WHITE, (x, 0), (x, WINDOW_HEIGHT))
+        for y in range(0, WINDOW_HEIGHT, CELL_HEIGHT):
+            pygame.draw.line(self.screen, WHITE, (0, y), (WINDOW_WIDTH, y))
 
-        pygame.display.update()
+    def draw(self):
+        self.screen.fill(BGCOLOR)
+        self.draw_grid()
+        pygame.display.flip()
+
+
+    def quit(self):
+        pygame.quit()
+        sys.exit()
+
+    def update(self):
+        pass
+
+g = Game()
+
+while True:
+    g.new()
+    g.run()
