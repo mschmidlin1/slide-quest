0e8d9584c1a6ed726621d733ee7a42e7d002c6e4
Maxwell Bourcier <maxwellbourcier@gmail.com>
2023-10-19 16:57:30 -0400

    notes for SlideQuest.py

--- SlideQuest.py
+++ SlideQuest.py
@@ -4,6 +4,9 @@ from modules.sprites import *
 import sys
 
 class Game:
+    """
+    Basic initialization of pygame necessary variables
+    """
     def __init__(self):
         pygame.init()
         pygame.display.set_caption(TITLE)
@@ -12,40 +15,65 @@ class Game:
         self.clock = pygame.time.Clock()
         self.load_data()
 
+
     def load_data(self):
-        #load map
+        """
+        load map --- this is where we can implement a text file with the exact desgn we want
+        """
         pass
 
+    
     def new(self):
+        """
+        Creation of all sprites, this is needed to initlaize all sprites given their (x, y) values and passed a state of the game object
+        This is run before the run function to make sure everything is initialized
+        """
         #initialization of all variables for new games
         self.all_sprites = pygame.sprite.Group()
         self.player = Player(self, 0, 0)
         pass
 
+    
     def run(self):
-        #game loop - set self.playing to False to end game
-
+        """
+        This is the main function for running all other events, updates and draw calls this is called within the main while loop
+        """
+        
         self.playing = True
+        #game loop - set self.playing to False to end game
 
         while self.playing:
             self.events()
             self.update()
             self.draw()
 
+    
     def events(self):
+        """
+        This is where we can implement player movement as well as menu interactions such as starting the game or quiting 
+        """
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.quit()
+
+    
     def draw_grid(self):
+        """
+        This is just temporary for showing the dimensions of the grid until we can start implementing sprites more regularly
+        """
         for x in range(0, WINDOW_WIDTH, CELL_WIDTH):
             pygame.draw.line(self.screen, WHITE, (x, 0), (x, WINDOW_HEIGHT))
         for y in range(0, WINDOW_HEIGHT, CELL_HEIGHT):
             pygame.draw.line(self.screen, WHITE, (0, y), (WINDOW_WIDTH, y))
 
     def draw(self):
+        """
+        Draw the sprite groups to the screen as well as handle the screen updating (pygame.display.flip())
+        """
         self.screen.fill(BGCOLOR)
         self.draw_grid()
         self.all_sprites.draw(self.screen)
+        #this is for updating the entire sceen instead of pygame.display.update which only updates a portion
         pygame.display.flip()
 
 
@@ -54,6 +82,10 @@ class Game:
         sys.exit()
 
     def update(self):
+        """
+        updating sprites specifically (moreso for movement) and will handle player screen camera movement 
+        if we want to implement procedural generation like lunatics
+        """
         self.all_sprites.update()
 
 g = Game()
