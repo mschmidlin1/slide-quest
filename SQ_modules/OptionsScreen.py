085e9586fdf3ede9c7ce3e642420932a86d4b7ee
Michael Schmidlin <mschmidlin1@gmail.com>
2024-04-06 15:40:35 -0400

    Slider class draft working

--- SQ_modules/OptionsScreen.py
+++ SQ_modules/OptionsScreen.py
@@ -4,11 +4,12 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import pygame
 from SQ_modules.configs import TITLE_FONT, WINDOW_DIMENSIONS, TITLE_SCREEN_TEXT_COLOR, LEFT_CLICK, TITLE_SCREEN_COLOR
 from SQ_modules.Sprites import TextSprite
-from SQ_modules.DataTypes import Point
+from SQ_modules.DataTypes import Point, Size
 from SQ_modules.Button import Button
 from SQ_modules.GameEnums import Screen
 from SQ_modules.Metas import SqScreenMeta
 from SQ_modules.NavigationManager import NavigationManager
+from SQ_modules.Slider import Slider
 
 class OptionsScreen(metaclass=SqScreenMeta):
     def __init__(self, screen):
@@ -27,21 +28,21 @@ class OptionsScreen(metaclass=SqScreenMeta):
             outline_width=1
             )
         
-        
+        self.title_screen_button: Button = Button(screen, (255,255,255), x=600, y=600, width=200, height=100, text="Title Screen", hover_color=(0,255,255))
 
         self.title_screen_sprite_group = pygame.sprite.Group()
         self.title_screen_sprite_group.add(self.title_sprite)
+
+        self.volume_slider = Slider(self.screen, Point(500, 500), (255, 0, 255), (255, 0, 0), Size(200, 5), Size(10, 20), label='volume slider')
 
     def update(self, events: list[pygame.event.Event]):
         """
         """
         self.title_screen_sprite_group.update()
+        self.title_screen_button.update(events)
+        self.volume_slider.update(events)
 
         for event in events:
-            # if event.type == pygame.MOUSEBUTTONDOWN:
-            #     if event.button == LEFT_CLICK:
-            #         if self.options_button.is_over(event.pos):
-            #             self.navigation_manager.navigate_to_options()
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE:
                     if not self.navigation_manager.game_active:
@@ -49,10 +50,17 @@ class OptionsScreen(metaclass=SqScreenMeta):
                     else:
                         self.navigation_manager.navigate_to(Screen.GAME)
 
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if event.button == LEFT_CLICK:
+                    if self.title_screen_button.is_over(event.pos):
+                        self.navigation_manager.navigate_to(Screen.TITLE)
 
     def draw(self):
         self.screen.fill(TITLE_SCREEN_COLOR)
         self.title_screen_sprite_group.draw(self.screen)
+        self.title_screen_button.draw()
+        self.volume_slider.draw()
+
         
 
 if __name__=="__main__":
---
+++ SQ_modules/Slider.py
@@ -0,0 +1,113 @@
+import pygame
+from SQ_modules.configs import LEFT_CLICK, GAME_VOLUME, TITLE_FONT
+from SQ_modules.DataTypes import Size, Point
+from SQ_modules.Sprites import TextSprite
+
+class Slider:
+    def __init__(self, screen, center_pos: Point, slider_color, knob_color, slider_size: Size, knob_size: Size, label: str = '', font = None, ):
+        """
+        x and y are the top left corner
+        """
+        self.screen = screen
+        self.center_pos = center_pos
+        self.slider_color = slider_color
+        self.knob_color = knob_color
+        self.slider_size = slider_size
+        self.knob_size = knob_size
+        self.label = label
+
+        self.font = font
+        if self.font is None:
+            self.font = font = pygame.font.SysFont('comicsans', 60)
+
+        self.dragging = False
+
+        self.current_slider_percent = GAME_VOLUME
+
+        top_left = (self.center_pos.x - (self.slider_size.width//2), self.center_pos.y - (self.slider_size.height//2))
+        width_height = (self.slider_size.width, self.slider_size.height)
+        self.slider_rect = pygame.Rect(top_left, width_height)
+
+        top_left = (self.center_pos.x - (self.knob_size.width//2), self.center_pos.y - (self.knob_size.height//2))
+        width_height = (self.knob_size.width, self.knob_size.height)
+        self.knob_rect = pygame.Rect(top_left, width_height)
+
+        self.percent_label_loc = Point(self.slider_rect.right+20, self.slider_rect.top)
+        self.percent_text_sprite = TextSprite(self.percent_str(), TITLE_FONT, 20, self.percent_label_loc, self.knob_color, anchor='center')
+
+        self.mouse_offset = (0, 0)
+
+        self.slider_sprite_group = pygame.sprite.Group()
+        self.slider_sprite_group.add(self.percent_text_sprite)
+
+    def percent_str(self) -> str:
+        """
+        Uses the current slider percent and returns a whole number percent 0-100 of where the slider currently is.
+        """
+        return str(int(round(self.current_slider_percent*100)))
+
+
+    def calculate_percent(self, x_px: int) -> float:
+        """
+        Takes in a pixel distance (relative to the start of the slider) and returns the percentage as a float [0-1]
+        """
+        percent_to_pixel = 1 / self.slider_size.width
+        return percent_to_pixel * x_px
+
+    def calculate_nearest_pixel_pos(self, percent: float) -> int:
+        """
+        Takes in a percentage of the slider and returns the pixel position of the middle of the knob to the nearest pixel.
+        """
+        pixel_position = percent * self.slider_size.width
+        return int(round(pixel_position))
+    
+    def is_over(self, pos) -> bool:
+        """
+        Checks if a mouse potition (pos) is over the button
+        """
+        
+        if self.knob_rect.left < pos[0] < self.knob_rect.right and self.knob_rect.top < pos[1] < self.knob_rect.bottom:
+            return True
+        else:
+            return False
+
+
+    def draw(self):
+        """
+        Call this method to draw the button on the screen
+        """
+
+        pygame.draw.rect(self.screen, self.slider_color, self.slider_rect)
+        pygame.draw.rect(self.screen, self.knob_color, self.knob_rect)
+        self.slider_sprite_group.draw(self.screen)
+
+    def update(self, events: list[pygame.event.Event]):
+        """
+        
+        """
+        self.percent_text_sprite.update_text(self.percent_str())
+        self.percent_text_sprite.update()
+
+
+        for event in events:
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if event.button == LEFT_CLICK:
+                    if self.is_over(event.pos):
+                        self.dragging = True
+                        self.mouse_offset = event.pos[0]-self.knob_rect.centerx
+            if event.type == pygame.MOUSEMOTION:
+                if self.dragging == True:
+                    new_x = event.pos[0] - self.mouse_offset
+                    #make sure the new position is inside the slider
+                    new_x = max(new_x, self.slider_rect.left)
+                    new_x = min(new_x, self.slider_rect.right)
+                    self.current_slider_percent = self.calculate_percent(new_x - self.slider_rect.left)
+                    move_distance = new_x - self.knob_rect.centerx
+                    self.knob_rect.move_ip(move_distance, 0)
+            if event.type == pygame.MOUSEBUTTONUP:
+                if event.button == LEFT_CLICK:
+                    self.dragging = False
+                    self.mouse_offset = 0
+
+
+        
